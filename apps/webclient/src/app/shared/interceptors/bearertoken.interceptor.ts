5a4ab7040c0244173502a5e55664d16d578768f0
Frank Ballmeyer <frank@ballmeyer.com>
2024-03-10 08:43:17 +0000

    feat: added ml service url for token interception

--- apps/webclient/src/app/shared/interceptors/bearertoken.interceptor.ts
+++ apps/webclient/src/app/shared/interceptors/bearertoken.interceptor.ts
@@ -19,7 +19,8 @@ export class BearerTokenInterceptor implements HttpInterceptor {
     if (url.startsWith(window.ENV.BALLWARE_METAURL.toLowerCase())
       || url.startsWith(window.ENV.BALLWARE_IDENTITYURL.toLowerCase())
       || url.startsWith(window.ENV.BALLWARE_DOCUMENTURL.toLowerCase())
-      || url.startsWith(window.ENV.BALLWARE_STORAGEURL.toLowerCase())) {
+      || url.startsWith(window.ENV.BALLWARE_STORAGEURL.toLowerCase())
+      || url.startsWith(window.ENV.BALLWARE_MLURL.toLowerCase())) {
         const token = this.authStorage.getItem('access_token');
         const header = 'Bearer ' + token;
         const headers = req.headers
