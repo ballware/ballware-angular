4bf2f7ea743e1f3e0e3787eda1785250cc782abc
Frank Ballmeyer <frank@ballmeyer.com>
2024-04-10 06:58:12 +0000

    fix: add option to set width on tabs edit layout item

--- libs/dx-renderer/src/lib/edit/tabs/tabs.component.ts
+++ libs/dx-renderer/src/lib/edit/tabs/tabs.component.ts
@@ -10,15 +10,18 @@ import { WithDestroy } from '../../utils/withdestroy';
 export class EditLayoutTabsComponent extends WithDestroy() implements OnChanges {
 
   private _height: string|undefined;
+  private _width: string|undefined;
   private _panels: EditLayoutItem[] = [];
 
   @Input() layoutItem!: EditLayoutItem;
 
   get panels() { return this._panels; }
   get height() { return this._height; }
+  get width() { return this._width; }
 
   ngOnChanges(): void {
     this._height = this.layoutItem.options?.height;
+    this._width = this.layoutItem.options?.width;
     this._panels = this.layoutItem.items?.filter(item => item.type === 'tab' && !item.ignore) ?? [];
   }
 }
