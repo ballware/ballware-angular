4a7d5b2c5713fd398c36caba6fb84d272845afd6
Frank Ballmeyer <frank.ballmeyer@assecosol.com>
2024-08-15 05:22:52 +0200

    fix failing responsive detector test

--- apps/webclient/src/app/shared/components/responsive-detector/responsive-detector.component.spec.ts
+++ apps/webclient/src/app/shared/components/responsive-detector/responsive-detector.component.spec.ts
@@ -1,20 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { Mock, Times } from 'moq.ts';
+
 import { ResponsiveDetectorComponent } from './responsive-detector.component';
+import { Provider } from '@angular/core';
+import { RESPONSIVE_SERVICE, ResponsiveService, SCREEN_SIZE } from '@ballware/meta-services';
 
 describe('ResponsiveDetectorComponent', () => {
   let component: ResponsiveDetectorComponent;
   let fixture: ComponentFixture<ResponsiveDetectorComponent>;
+  const svcMock = new Mock<ResponsiveService>()
+    .setup(instance => instance.onResize(SCREEN_SIZE.XS)).returns(true as any);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ResponsiveDetectorComponent ]
+      declarations: [ ResponsiveDetectorComponent ],
+      providers: [
+        {
+          provide: RESPONSIVE_SERVICE,
+          useValue: svcMock.object()
+        } as Provider
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(ResponsiveDetectorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    svcMock.verify(instance => {
+      instance.onResize(SCREEN_SIZE.XS)
+    }, Times.Once());
   });
 
   it('should create', () => {
