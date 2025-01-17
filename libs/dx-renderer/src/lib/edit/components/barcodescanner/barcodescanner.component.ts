23898c705e34538ecf2543385241e87349444f70
Frank Ballmeyer <frank@ballmeyer.com>
2025-01-17 12:47:45 +0100

    fix: Better camera support and auto disable on scan end

--- libs/dx-renderer/src/lib/edit/components/barcodescanner/barcodescanner.component.ts
+++ libs/dx-renderer/src/lib/edit/components/barcodescanner/barcodescanner.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from "@angular/common";
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, Output, SimpleChanges, ViewChild } from "@angular/core";
 import { ValueType } from "@ballware/meta-model";
 import { BarcodeFormat, Result } from "@zxing/library";
-import { ZXingScannerModule } from "@zxing/ngx-scanner";
+import { ZXingScannerComponent, ZXingScannerModule } from "@zxing/ngx-scanner";
 import { I18NextModule } from "angular-i18next";
 import { DxButtonModule, DxSelectBoxModule, DxToolbarModule } from "devextreme-angular";
+import { SelectionChangedEvent } from "devextreme/ui/select_box";
 
 @Component({
     selector: 'ballware-barcodescanner',
@@ -13,17 +14,35 @@ import { DxButtonModule, DxSelectBoxModule, DxToolbarModule } from "devextreme-a
     imports: [CommonModule, I18NextModule, DxToolbarModule, DxSelectBoxModule, DxButtonModule, ZXingScannerModule],
     standalone: true
 })
-export class BarcodeScannerComponent {
+export class BarcodeScannerComponent implements OnDestroy, OnChanges {
     
+    @ViewChild('scanner', { static: false }) scanner?: ZXingScannerComponent;
+
+    @Input() enabled!: boolean;
     @Input() width: string|undefined;
     @Input() height: string|undefined;
     
     @Output() valueChange = new EventEmitter<ValueType>();
     
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['enabled']) {
+            this.enabled = changes['enabled'].currentValue;
+        }
+    }
+
+    ngOnDestroy(): void {
+        if (this.scanner) {
+            this.scanner.torch = false;
+            this.scanner.enable = false;
+        }
+    }
+
     public devices: Array<MediaDeviceInfo> = [];
     public activeDevice: MediaDeviceInfo|undefined = undefined;
     public torchCompatible = false;
     public torchEnabled = false;
+
+    public autofocusEnabled = false;
 
     public allowedFormats = [
         BarcodeFormat.AZTEC, 
@@ -55,16 +74,25 @@ export class BarcodeScannerComponent {
         this.torchEnabled = !this.torchEnabled;
     }
 
+    public onAutofocusClicked() {        
+        this.autofocusEnabled = !this.autofocusEnabled;
+    }
+
     public onCamerasFound(devices: Array<MediaDeviceInfo>) {
         console.log('onCamerasFound', devices);
         this.devices = devices;
-        this.activeDevice = this.devices?.length ? this.devices[0] : undefined;
     }
 
     public onCamerasNotFound() {
         console.log('onCamerasNotFound');
     }
 
+    public onDeviceChange(device: MediaDeviceInfo) {
+        if (this.activeDevice !== device) {
+            this.activeDevice = device;
+        }
+    }
+    
     public onScanError(error: Error) {
         console.log(`onScanError: ${error}`);
     }
