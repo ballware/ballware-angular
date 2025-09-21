9d2d4da329a2d4539f8a7cdf9d85a05abdffa10f
Frank Ballmeyer <frank@ballmeyer.com>
2025-09-21 13:24:17 +0200

    Reduce signaling of screen size changes #37

--- libs/common-meta-services/src/lib/responsive.service.ts
+++ libs/common-meta-services/src/lib/responsive.service.ts
@@ -1,18 +1,21 @@
 import { ResponsiveService, SCREEN_SIZE } from "@ballware/meta-services";
 import { distinctUntilChanged, map, Observable, shareReplay, tap } from "rxjs";
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 export class DefaultResponsiveService implements ResponsiveService {
 
-  constructor(private bp: BreakpointObserver) {}
+  onResize$: Observable<SCREEN_SIZE>;
 
-  get onResize$(): Observable<SCREEN_SIZE> {
-    return this.bp.observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large, Breakpoints.XLarge]).pipe(
+  constructor(private bp: BreakpointObserver) {
+    this.onResize$ = this.bp.observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large, Breakpoints.XLarge]).pipe(
+      takeUntilDestroyed(),
       map(s => s.breakpoints[Breakpoints.XSmall] ? SCREEN_SIZE.XS :
-              s.breakpoints[Breakpoints.Small]  ? SCREEN_SIZE.SM :
-              s.breakpoints[Breakpoints.Medium] ? SCREEN_SIZE.MD :
-              s.breakpoints[Breakpoints.Large] ? SCREEN_SIZE.LG : 
+        s.breakpoints[Breakpoints.Small]  ? SCREEN_SIZE.SM :
+          s.breakpoints[Breakpoints.Medium] ? SCREEN_SIZE.MD :
+            s.breakpoints[Breakpoints.Large] ? SCREEN_SIZE.LG :
               SCREEN_SIZE.LG),
+      distinctUntilChanged(),
       tap(s => console.log('Screen size:', SCREEN_SIZE[s])),
       shareReplay({ bufferSize: 1, refCount: true })
     )
