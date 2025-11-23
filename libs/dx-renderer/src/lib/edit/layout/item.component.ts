87fdd57c3f5ffa314f7ece629fda6472cadeaccd
Frank Ballmeyer <frank@ballmeyer.com>
2025-11-23 10:50:36 +0100

    Dropped redundant lookup edit item implementations #48

--- libs/dx-renderer/src/lib/edit/layout/item.component.ts
+++ libs/dx-renderer/src/lib/edit/layout/item.component.ts
@@ -14,8 +14,6 @@ import { EditLayoutMultivalueComponent } from '../multivalue/multivalue.componen
 import { EditLayoutNumberComponent } from '../number/number.component';
 import { EditLayoutSqlComponent } from '../sql/sql.component';
 import { EditLayoutStaticButtonGroupComponent } from '../staticbuttongroup/staticbuttongroup.component';
-import { EditLayoutStaticlookupComponent } from '../staticlookup/staticlookup.component';
-import { EditLayoutStaticmultilookupComponent } from '../staticmultilookup/staticmultilookup.component';
 import { EditLayoutStatisticComponent } from '../statistic/statistic.component';
 import { EditLayoutTabsComponent } from '../tabs/tabs.component';
 import { EditLayoutTextComponent } from '../text/text.component';
@@ -35,7 +33,7 @@ import { CommonModule } from '@angular/common';
 })
 export class EditLayoutItemComponent implements AfterViewInit {
 
-  @ViewChild('item', { read: ViewContainerRef }) private itemHost?: ViewContainerRef;
+  @ViewChild('item', { read: ViewContainerRef }) private readonly itemHost?: ViewContainerRef;
 
   @Input() layoutItem!: EditLayoutItem;
 
@@ -46,193 +44,170 @@ export class EditLayoutItemComponent implements AfterViewInit {
         case 'button': {
             const componentRef = this.itemHost.createComponent(EditLayoutButtonComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem);            
-            componentRef.changeDetectorRef.detectChanges();            
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
+            componentRef.changeDetectorRef.detectChanges();
           }
           break;
         case 'staticbuttongroup': {
             const componentRef = this.itemHost.createComponent(EditLayoutStaticButtonGroupComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem);            
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
           }
-          break;          
+          break;
         case 'text':
         case 'mail': {
-            //const { EditLayoutTextComponent } = await import('../text/text.component');
             const componentRef = this.itemHost.createComponent(EditLayoutTextComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem);            
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
           }
           break;
         case 'textarea': {
-            //const { EditLayoutTextareaComponent } = await import('../textarea/textarea.component');
             const componentRef = this.itemHost.createComponent(EditLayoutTextareaComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem);            
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
           }
           break;
         case 'richtext':  {
             const componentRef = this.itemHost.createComponent(EditLayoutRichtextComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem);            
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
-          }                        
-          break;          
+          }
+          break;
         case 'number': {
-            //const { EditLayoutNumberComponent } = await import('../number/number.component');
             const componentRef = this.itemHost.createComponent(EditLayoutNumberComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem);            
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
           }
           break;
         case 'bool': {
             const componentRef = this.itemHost.createComponent(EditLayoutBoolComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem);            
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
-          }                        
+          }
           break;
         case 'toggle': {
             const componentRef = this.itemHost.createComponent(EditLayoutToggleComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem);            
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
-          }                        
-          break;          
+          }
+          break;
         case 'date':
         case 'datetime': {
-            //const { EditLayoutDatetimeComponent } = await import('../datetime/datetime.component');
             const componentRef = this.itemHost.createComponent(EditLayoutDatetimeComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem);            
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
           }
           break;
         case 'lookup':
+        case 'staticlookup':
         case 'pickvalue': {
-            //const { EditLayoutLookupComponent } = await import('../lookup/lookup.component');
             const componentRef = this.itemHost.createComponent(EditLayoutLookupComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem);            
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
           }
           break;
-        case 'multilookup': {
-            //const { EditLayoutMultilookupComponent } = await import('../multilookup/multilookup.component');
+        case 'multilookup':
+        case 'staticmultilookup':{
             const componentRef = this.itemHost.createComponent(EditLayoutMultilookupComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem);            
-            componentRef.changeDetectorRef.detectChanges();
-          }
-          break;
-        case 'staticlookup': {
-            //const { EditLayoutStaticlookupComponent } = await import('../staticlookup/staticlookup.component');
-            const componentRef = this.itemHost.createComponent(EditLayoutStaticlookupComponent);
-
-            componentRef.setInput('initialLayoutItem', this.layoutItem);            
-            componentRef.changeDetectorRef.detectChanges();
-          }
-          break;
-        case 'staticmultilookup': {
-            //const { EditLayoutStaticmultilookupComponent } = await import('../staticmultilookup/staticmultilookup.component');
-            const componentRef = this.itemHost.createComponent(EditLayoutStaticmultilookupComponent);
-
-            componentRef.setInput('initialLayoutItem', this.layoutItem);            
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
           }
           break;
         case 'multivalue': {
             const componentRef = this.itemHost.createComponent(EditLayoutMultivalueComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem);            
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
           }
           break;
         case 'tabs': {
-            //const { EditLayoutTabsComponent } = await import('../tabs/tabs.component');
             const componentRef = this.itemHost.createComponent(EditLayoutTabsComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem);            
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
           }
           break;
         case 'group': {
-            //const { EditLayoutGroupComponent } = await import('../group/group.component');
             const componentRef = this.itemHost.createComponent(EditLayoutGroupComponent);
 
-            componentRef.setInput('layoutItem', this.layoutItem);            
+            componentRef.setInput('layoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
           }
           break;
         case 'map':  {
             const componentRef = this.itemHost.createComponent(EditLayoutMapComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem);            
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
-          }                        
+          }
           break;
         case 'entitygrid': {
-            //const { EditLayoutEntitygridComponent } = await import('../entitygrid/entitygrid.component');
             const componentRef = this.itemHost.createComponent(EditLayoutEntitygridComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem); 
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
           }
           break;
         case 'detailgrid': {
             const componentRef = this.itemHost.createComponent(EditLayoutDetailGridComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem); 
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
           }
           break;
         case 'detailtree': {
             const componentRef = this.itemHost.createComponent(EditLayoutDetailTreeComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem); 
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
           }
-          break;          
+          break;
         case 'json': {
             const componentRef = this.itemHost.createComponent(EditLayoutJsonComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem); 
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
           }
           break;
         case 'javascript': {
             const componentRef = this.itemHost.createComponent(EditLayoutJavascriptComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem); 
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
           }
-          break;          
+          break;
         case 'sql': {
             const componentRef = this.itemHost.createComponent(EditLayoutSqlComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem); 
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
           }
-          break;          
+          break;
         case 'attachements':
         case 'attachments': {
             const componentRef = this.itemHost.createComponent(EditLayoutAttachmentsComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem); 
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
-          } 
-          break;  
+          }
+          break;
         case 'statistic': {
             const componentRef = this.itemHost.createComponent(EditLayoutStatisticComponent);
 
-            componentRef.setInput('initialLayoutItem', this.layoutItem); 
+            componentRef.setInput('initialLayoutItem', this.layoutItem);
             componentRef.changeDetectorRef.detectChanges();
-          } 
-          break;            
+          }
+          break;
         case 'empty':
           break;
         default: {
