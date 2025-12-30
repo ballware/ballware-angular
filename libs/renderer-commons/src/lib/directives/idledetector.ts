3bd333c3ae68562d2dcb56749795516022e7824d
Frank Ballmeyer <frank@ballmeyer.com>
2025-12-30 08:54:42 +0100

    Replace direct document reference in idle detector with DOCUMENT injection token #52

--- libs/renderer-commons/src/lib/directives/idledetector.ts
+++ libs/renderer-commons/src/lib/directives/idledetector.ts
@@ -1,12 +1,17 @@
-import { Directive, Inject, OnDestroy, OnInit } from "@angular/core";
+import { Directive, Inject, OnDestroy, OnInit, PLATFORM_ID } from "@angular/core";
+import { DOCUMENT, isPlatformBrowser } from "@angular/common";
 import { IDLE_SERVICE, IdleService } from "@ballware/meta-services";
 
 @Directive({
   standalone: true
 })
 export class IdleDetector implements OnInit, OnDestroy {
-  
-  constructor(@Inject(IDLE_SERVICE) private readonly idleService: IdleService) {
+
+  constructor(
+    @Inject(PLATFORM_ID) private readonly platformId: object,
+    @Inject(DOCUMENT) private readonly document: Document,
+    @Inject(IDLE_SERVICE) private readonly idleService: IdleService
+  ) {
     this.triggerBusy = this.triggerBusy.bind(this);
   }
 
@@ -15,20 +20,24 @@ export class IdleDetector implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    console.log('Registering busy events');
-    document.addEventListener('mousemove', this.triggerBusy);
-    document.addEventListener('mousedown', this.triggerBusy); 
-    document.addEventListener('keypress', this.triggerBusy);
-    document.addEventListener('touchstart', this.triggerBusy);
+    if (isPlatformBrowser(this.platformId)) {
+      console.log('Registering busy events');
+      this.document.addEventListener('mousemove', this.triggerBusy);
+      this.document.addEventListener('mousedown', this.triggerBusy);
+      this.document.addEventListener('keypress', this.triggerBusy);
+      this.document.addEventListener('touchstart', this.triggerBusy);
 
-    this.idleService.renewBusy();
+      this.idleService.renewBusy();
+    }
   }
 
   ngOnDestroy(): void {
-    console.log('Unregistering busy events');
-    document.removeEventListener('mousemove', this.triggerBusy);
-    document.removeEventListener('mousedown', this.triggerBusy); 
-    document.removeEventListener('keypress', this.triggerBusy);
-    document.removeEventListener('touchstart', this.triggerBusy);
+    if (isPlatformBrowser(this.platformId)) {
+      console.log('Unregistering busy events');
+      this.document.removeEventListener('mousemove', this.triggerBusy);
+      this.document.removeEventListener('mousedown', this.triggerBusy);
+      this.document.removeEventListener('keypress', this.triggerBusy);
+      this.document.removeEventListener('touchstart', this.triggerBusy);
+    }
   }
 }
